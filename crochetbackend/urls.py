9a9d6122044f31f4b082f3480b5290520c0364f4
unseen-Programmer <programmerunseen@gmail.com>
2025-12-11 03:42:21 +0530

    Add API home route

--- crochetbackend/urls.py
+++ crochetbackend/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({
+        "status": "ANE Crochet API Running Successfully 🚀",
+        "admin": "/admin/",
+        "products": "/api/products/"
+    })
 
 urlpatterns = [
+    path("", home),
     path("admin/", admin.site.urls),
-    path("api/", include("shop.urls")),   # ✅ THIS LINE FIXES YOUR ERROR
+    path("api/", include("shop.urls")),
 ]
