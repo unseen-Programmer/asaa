469ae97baeae305f0acc4cbd9fc4a737cac49863
unseen-Programmer <programmerunseen@gmail.com>
2025-12-11 15:16:02 +0530

    Fix Cloudinary full image URL in serializer

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
-from .models import Product, Category, ProductImage
-
+from .models import Product, ProductImage, Category
+import cloudinary
 
 class ProductImageSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = ProductImage
         fields = ["image"]
+
+    def get_image(self, obj):
+        if obj.image:
+            return cloudinary.utils.cloudinary_url(obj.image.name)[0]
+        return None
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -15,13 +22,19 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    images = ProductImageSerializer(many=True, read_only=True)
     category = CategorySerializer()
-    images = ProductImageSerializer(many=True)
 
     class Meta:
         model = Product
         fields = [
-            "id", "name", "slug", "category",
-            "price", "stock", "description",
-            "images", "view_count",
+            "id",
+            "name",
+            "slug",
+            "category",
+            "price",
+            "stock",
+            "description",
+            "images",
+            "view_count",
         ]
