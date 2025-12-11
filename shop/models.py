e6b8b0aff22aaaa855cad426319f9113221dae73
unseen-Programmer <programmerunseen@gmail.com>
2025-12-11 15:06:01 +0530

    Fix models.py circular import

--- shop/models.py
+++ shop/models.py
@@ -1,24 +1,32 @@
-from rest_framework import serializers
-from .models import Product, ProductImage, Category
+from django.db import models
+from cloudinary.models import CloudinaryField
 
-class ProductImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ProductImage
-        fields = ["image"]
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
 
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ["id", "name"]
+    def __str__(self):
+        return self.name
 
-class ProductSerializer(serializers.ModelSerializer):
-    images = ProductImageSerializer(many=True, read_only=True)
-    category = CategorySerializer()
 
-    class Meta:
-        model = Product
-        fields = [
-            "id", "name", "slug", "category",
-            "price", "stock", "description",
-            "images", "view_count"
-        ]
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField(default=0)
+    description = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    view_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="images")
+    image = CloudinaryField("image")   # Uploads to Cloudinary
+
+    def __str__(self):
+        return f"Image for {self.product.name}"
